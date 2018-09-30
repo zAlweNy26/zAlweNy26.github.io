1bbc3184b7867be8705255dd9e5322fd83363f86
AlweProjects <zdanymc@gmail.com>
2018-09-30 11:18:24 +0200

    Alwe Profile Web Page Files

--- js/script.js
+++ js/script.js
@@ -57,24 +57,22 @@ setInterval(function () {
   });
 }, 1000);
 
-setInterval(function () {
-  $.getJSON("https://api.github.com/users/zAlweNy26?client_id=" + keys.GITHUBID + "&client_secret=" + keys.GITHUB, function (git) {
-    current2 = JSON.stringify(git);
-    if (previous2 && current2 && previous2 !== current2) $('.git-content').load(document.URL + ' .git-content');
-    previous2 = current2;
-    $('.giturl').attr("href", git.html_url);
-    $('#giticon').attr("src", git.avatar_url);
-    $('#gitnick').html(git.login);
-    $('#gitfollowers').html("<a class='gitlink' href='https://github.com/" + git.login + "?tab=followers' target='_blank'> Followers</a><br>" + git.followers);
-    $('#gitfollowing').html("<a class='gitlink' href='https://github.com/" + git.login + "?tab=following' target='_blank'> Following</a><br>" + git.following);
-    $('#gitrepos').html("<a class='gitlink' href='https://github.com/" + git.login + "?tab=repositories' target='_blank'> Repositories</a><br>" + git.public_repos);
-  });
-}, 1000);
+$.getJSON("https://api.github.com/users/zAlweNy26?client_id=" + keys.GITHUBID + "&client_secret=" + keys.GITHUB, function (git) {
+  current2 = JSON.stringify(git);
+  if (previous2 && current2 && previous2 !== current2) $('.git-content').load(document.URL + ' .git-content');
+  previous2 = current2;
+  $('.giturl').attr("href", git.html_url);
+  $('#giticon').attr("src", git.avatar_url);
+  $('#gitnick').html(git.login);
+  $('#gitfollowers').html("<a class='gitlink' href='https://github.com/" + git.login + "?tab=followers' target='_blank'> Followers</a><br>" + git.followers);
+  $('#gitfollowing').html("<a class='gitlink' href='https://github.com/" + git.login + "?tab=following' target='_blank'> Following</a><br>" + git.following);
+  $('#gitrepos').html("<a class='gitlink' href='https://github.com/" + git.login + "?tab=repositories' target='_blank'> Repositories</a><br>" + git.public_repos);
+});
 
 $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername=PlayerInsideIT&key=" + keys.GOOGLE, function (yt) {
   $('.yturl').attr("href", "https://www.youtube.com/channel/" + yt.items[0].id);
   $('#yticon').attr("src", yt.items[0].snippet.thumbnails.medium.url);
-  $('#ytnick').html(yt.items[0].snippet.title + " <span id='favyt'><span class='redbra'>[</span> Favourite Youtuber <span class='redbra'>]</span></span>");
+  $('#ytnick').html("<span id='favyt'>Favourite Youtuber</span><br>" + yt.items[0].snippet.title);
   let a = new Date(yt.items[0].snippet.publishedAt);
   let year = a.getFullYear(), month = a.getMonth(), day = a.getDate();
   if (day < 10) day = "0" + day;
@@ -84,5 +82,19 @@ $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatist
   $('#ytsubs').html("<span style='color: rgb(225, 0, 0)'>Subscribers</span><br>" + parseInt(yt.items[0].statistics.subscriberCount).toLocaleString());
   $('#ytvideos').html("<span style='color: rgb(225, 0, 0)'>Uploads</span><br>" + parseInt(yt.items[0].statistics.videoCount).toLocaleString());
   $('#ytviews').html("<span style='color: rgb(225, 0, 0)'>Views</span><br>" + parseInt(yt.items[0].statistics.viewCount).toLocaleString());
-  $('#ytsince').html("<span style='color: rgb(225, 0, 0)'>Created at</span><br>" + created);
+});
+
+$.getJSON("https://kitsu.io/api/edge/anime?filter[text]=trigun", function (anime) {
+  $('.animeurl').attr("href", "https://kitsu.io/anime/trigun" + anime.data[0].attributes.slug);
+  $('#animeicon').attr("src", anime.data[0].attributes.posterImage.tiny);
+  $('#animenick').html("<span id='favanime'>Favourite Anime</span><br>" + anime.data[0].attributes.titles.en);
+  $('#animerate').html("<span style='color: rgb(225, 0, 0)'>Rating</span><br>" + anime.data[0].attributes.averageRating + " %");
+  $('#animestart').html("<span style='color: rgb(225, 0, 0)'>Start Date</span><br>" + anime.data[0].attributes.startDate);
+  $('#animeend').html("<span style='color: rgb(225, 0, 0)'>End Date</span><br>" + anime.data[0].attributes.endDate);
+  $('#animeepisodes').html("<span style='color: rgb(225, 0, 0)'>Episodes</span><br>" + anime.data[0].attributes.episodeCount);
+  $('#animeeplength').html("<span style='color: rgb(225, 0, 0)'>Episodes Length</span><br>" + anime.data[0].attributes.episodeLength + " minutes");
+  let nsfw;
+  if (anime.data[0].attributes.nsfw === "true") nsfw = "Yes";
+  else nsfw = "No";
+  $('#animensfw').html("<span style='color: rgb(225, 0, 0)'>NSFW</span><br>" + nsfw);
 });
