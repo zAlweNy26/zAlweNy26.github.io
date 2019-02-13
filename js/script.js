5b1b99915634aa0cd544d19a59177619260de963
AlweProjects <zdanymc@gmail.com>
2019-02-13 16:22:43 +0100

    Updated Files

--- js/script.js
+++ js/script.js
@@ -4,16 +4,20 @@ var keys = {
   TWITCH: CryptoJS.AES.decrypt("U2FsdGVkX1/scalSQEqycGqJ8B6uFGqAU94VsXU1///NfZlSM4V1vsipO7Hfw0fP", "Alwe").toString(CryptoJS.enc.Utf8),
 }
 
-var previous = null, current = null, previous1 = null, current1 = null, previous2 = null, current2 = null, previous3 = null, current3 = null;
+var prv1 = null, crt1 = null, prv2 = null, crt2 = null, prv3 = null, crt3 = null;
+var prv4 = null, crt4 = null, prv5 = null, crt5 = null, prv6 = null, crt6 = null, prv7 = null, crt7 = null;
 
 $.ajax({
   url: "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=" + keys.STEAM + "&steamids=76561198154675159",
   type: 'GET',
   dataType: 'json',
+  beforeSend: function (xhr) {
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+  },
   success: function (steam) {
-    current = JSON.stringify(steam);
-    if (previous && current && previous !== current) $('.steam-content').load(document.URL + ' .steam-content');
-    previous = current;
+    crt1 = JSON.stringify(steam);
+    if (prv1 && crt1 && prv1 !== crt1) $('.steam-content').load(document.URL + ' .steam-content');
+    prv1 = crt1;
     $('.steamurl').attr("href", steam.response.players[0].profileurl);
     $('#steamicon').attr("src", steam.response.players[0].avatarfull);
     alert(steam.response.players[0].avatarfull);
@@ -47,22 +51,20 @@ $.ajax({
     }
     $('#steamicon').css({ "border": "2px solid " + status });
   },
-  error: function () { },
-  beforeSend: function (xhr) { 
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*'); 
-    xhr.setRequestHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
-    xhr.setRequestHeader('Access-Control-Allow-Credentials', true); 
-  }
+  error: function () { }
 });
 
 $.ajax({
   url: "https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key=" + keys.STEAM + "&steamid=76561198154675159",
   type: 'GET',
   dataType: 'json',
+  beforeSend: function (xhr) {
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+  },
   success: function (steam) {
-    current1 = JSON.stringify(steam);
-    if (previous1 && current1 && previous1 !== current1) $('#steamlevel').load(document.URL + ' #steamlevel');
-    previous1 = current1;
+    crt2 = JSON.stringify(steam);
+    if (prv2 && crt2 && prv2 !== crt2) $('#steamlevel').load(document.URL + ' #steamlevel');
+    prv2 = crt2;
     let colors = ["rgb(155, 155, 155)", "rgb(190, 40, 65)", "rgb(215, 90, 65)", "rgb(255, 205, 35)",
       "rgb(70, 120, 60)", "rgb(80, 140, 220)", "rgb(120, 80, 200)", "rgb(195, 80, 200)", 
       "rgb(85, 35, 55)", "rgb(155, 125, 80)", "rgb(155, 155, 155)"], colorlevel = null, level = steam.response.player_level;
@@ -81,12 +83,7 @@ $.ajax({
     }
     $('#steamlevel').html(level);
   },
-  error: function () { },
-  beforeSend: function (xhr) {
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xhr.setRequestHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
-    xhr.setRequestHeader('Access-Control-Allow-Credentials', true); 
-  }
+  error: function () { }
 });
 
 $.ajax({
@@ -94,9 +91,9 @@ $.ajax({
   type: 'GET',
   dataType: 'json',
   success: function (yt) {
-    current2 = JSON.stringify(yt);
-    if (previous2 && current2 && previous2 !== current2) $('.yt-content').load(document.URL + ' .yt-content');
-    previous2 = current2;
+    crt3 = JSON.stringify(yt);
+    if (prv3 && crt3 && prv3 !== crt3) $('.yt-content').load(document.URL + ' .yt-content');
+    prv3 = crt3;
     $('.yturl').attr("href", "https://www.youtube.com/channel/" + yt.items[0].id);
     $('#yticon').attr("src", yt.items[0].snippet.thumbnails.medium.url);
     $('#ytnick').html("<span id='favyt'>Favourite Youtuber</span><br>" + yt.items[0].snippet.title);
@@ -112,6 +109,9 @@ $.ajax({
   type: 'GET',
   dataType: 'json',
   success: function (anime) {
+    crt4 = JSON.stringify(anime);
+    if (prv4 && crt4 && prv4 !== crt4) $('.anime-content').load(document.URL + ' .anime-content');
+    prv4 = crt4;
     $('.animeurl').attr("href", "https://kitsu.io/anime/" + anime.data[0].attributes.slug);
     $('#animeicon').attr("src", anime.data[0].attributes.posterImage.tiny);
     $('#animenick').html("<span id='favanime'>Favourite Anime</span><br>" + anime.data[0].attributes.titles.en);
@@ -135,6 +135,9 @@ $.ajax({
   type: 'GET',
   dataType: 'json',
   success: function (data) {
+    crt5 = JSON.stringify(data);
+    if (prv5 && crt5 && prv5 !== crt5) $('.twitch-content').load(document.URL + ' .twitch-content');
+    prv5 = crt5;
     $('#twitchicon').attr("src", data.data[0].profile_image_url);
     $('#streamer').html(data.data[0].display_name);
     $('#streamerviews').html(parseInt(data.data[0].view_count).toLocaleString() + "<span style='color: rgb(100, 65, 165)'> total views</span>");
@@ -148,6 +151,9 @@ $.ajax({
   type: 'GET',
   dataType: 'json',
   success: function (data) {
+    crt6 = JSON.stringify(data);
+    if (prv6 && crt6 && prv6 !== crt6) $('.twitch-content').load(document.URL + ' .twitch-content');
+    prv6 = crt6;
     $('#streamerfollowers').html(parseInt(data.total).toLocaleString() + "<span style='color: rgb(100, 65, 165)'> total followers</span>");
   },
   error: function () { },
@@ -159,6 +165,9 @@ $.ajax({
   type: 'GET',
   dataType: 'json',
   success: function (data) {
+    crt7 = JSON.stringify(data);
+    if (prv7 && crt7 && prv7 !== crt7) $('.twitch-content').load(document.URL + ' .twitch-content');
+    prv7 = crt7;
     if (data.data.length === 0) {
       $('#streamerplaying').html("Offline");
     } else {
