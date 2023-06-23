2ef242ec983720379f661317327c892d706ef153
Dany <zdanymc@gmail.com>
2023-06-23 21:56:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/zalweny26.github.io/',
+  base: '/',
   plugins: [
     vue(),
     AutoImport({
