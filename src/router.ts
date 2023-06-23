d7fbff0efd259c8d43dd81bc6548087b9de1927a
Dany <zdanymc@gmail.com>
2023-06-23 21:53:40 +0200

    changed paths

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   linkActiveClass: "active",
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/zalweny26.github.io/',
   plugins: [
     vue(),
     AutoImport({
