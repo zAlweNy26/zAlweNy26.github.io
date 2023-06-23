cea26c2228f5fd873490e0eb61129ee188737485
Dany <zdanymc@gmail.com>
2023-06-23 22:16:56 +0200

    added workaround

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   linkActiveClass: "active",
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
