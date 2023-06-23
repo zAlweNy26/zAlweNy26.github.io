9fcd55c11261c425b136d0b6400b16a00f4a9fca
Dany <zdanymc@gmail.com>
2023-06-23 22:08:23 +0200

    try hash history

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
   linkActiveClass: "active",
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
